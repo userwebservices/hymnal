455658d088e1bb29db2ea82fac33089ab4b13ce9
saul <userwebservices@gmail.com>
2025-10-12 16:48:42 -0600

    changes for sukkot

--- js/axios.js
+++ js/axios.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Immediately set welcome view on initial load
     displaySection.classList.add('welcome-view');
 
-    displaySection.style.backgroundImage = 'url("../../assets/bg/cover/sukkot_02.webp")'; //Maneja la FOTO INICIAL
+    displaySection.style.backgroundImage = 'url("../../assets/bg/cover/sukkah.webp")'; //Maneja la FOTO INICIAL
  
     
     
